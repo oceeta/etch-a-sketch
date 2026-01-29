9d8fd830066a8037e0bc0b0b60d09e005b4c352e
Osita Nwajah <101451314+oceeta@users.noreply.github.com>
2026-01-29 12:57:04 +0100

    Add function to draw flexbox grid

---
+++ scripts/script.js
@@ -0,0 +1,28 @@
+function drawGrid(squares) {
+    const container = document.querySelector("#container");
+
+    // Draw rows
+    for (let i = 0; i < squares; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+        row.setAttribute("id", `row${i}`);
+        container.appendChild(row);
+
+        // Draw columns
+        for (let j = 0; j < squares; j++) {
+            const col = document.createElement("div");
+            col.classList.add("col");
+            col.classList.add(`col${j}`);
+            row.appendChild(col);
+        }
+    }
+
+    // Remove bottom border on last row and right border on last columns
+    const lastRow = document.querySelector(`#row${squares - 1}`);
+    lastRow.style.borderBottomWidth = "0";
+
+    const lastCols = document.querySelectorAll(`.col${squares - 1}`);
+    lastCols.forEach((col) => col.style.borderRightWidth = "0");
+}
+
+drawGrid(16);
